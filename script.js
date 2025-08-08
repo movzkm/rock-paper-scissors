17ad6c23a535e63deea4e4e8acb346b69a2faf10
movzkm <movzkm@gmail.com>
2025-08-08 11:05:54 +0300

    project setup

---
+++ script.js
@@ -0,0 +1,31 @@
+
+const rock = 0;
+const paper = 1;
+const scissors = 2;
+
+const winningMove = [scissors, rock, paper];
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+function getHumanChoice() {
+
+}
+
+function getComputerChoice() {
+
+}
+
+function playRound(humanChoice, computerChoice) {
+
+}
+
+
+/*
+
+1- you get three moves
+2- the move is used as an index
+3- the value of each index (move) represents the move it beats
+
+*/

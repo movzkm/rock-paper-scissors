72cf40ebaf8f76eb9d3a0b3ab5933a3b7329c2ef
movzkm <movzkm@gmail.com>
2025-08-08 11:18:46 +0300

    get the human and computer choices

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const paper = 1;
 const scissors = 2;
 
 const winningMove = [scissors, rock, paper];
+const movesAsStrings = ["rock", "paper", "scissors"];
+const movesNumber = 3;
 
 let humanScore = 0;
 let computerScore = 0;
@@ -11,16 +13,24 @@ let computerScore = 0;
 
 function getHumanChoice() {
 
+    let choice = prompt("Rock-Paper-Scissors").toLowerCase();
+
+    for (let i = 0; i < movesNumber; i += 1) {
+        if (choice == movesAsStrings[i]) {
+            return i;
+        }
+    }
 }
 
 function getComputerChoice() {
-
+    return Math.floor((Math.random() * 10) % 3);
 }
 
 function playRound(humanChoice, computerChoice) {
 
 }
 
+console.log(getHumanChoice());
 
 /*
 
